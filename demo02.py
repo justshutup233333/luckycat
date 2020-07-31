57264d9b39ed6725ee844dfe132ce221e22ca638
morning <3225955804@qq.com>
2020-07-31 22:42:54 +0800

    luckycat

--- demo02.py
+++ demo02.py
@@ -26,7 +26,9 @@ print(c)
 a.remove("沈周")
 del a[0]
 print(a)
-'''
+
+
+
 d={
     "name":("aa","bb","cc","dd"),
     "age":[3,7,6,80],
@@ -44,3 +46,59 @@ d.update(name11=piupiu)
 print(d)
 del d["name11"]
 print(d)
+
+
+
+
+type(2333)
+# print(type(23333))数据格式的转换
+print(type("morning"))
+print(type(233.0))
+print(type("沈周"))
+print(type(True))
+print(type(()))
+print(type([]))
+# print(type({}))要被包裹起来
+
+a=10
+b=11
+if a>b:
+    print(a)
+else:
+    print(b)
+
+username=input("请输入用户名：")
+passnumber=input("请输入密码")
+if username=userinfo.get("username")
+
+
+username=input("请输入用户名：")
+passnumber=input("请输入密码: ")
+a=len(username)
+b=len(passnumber)
+if a>5:
+    if b>8:
+        print("账号注册成功")
+    else:
+        print("密码格式错误")
+else:
+    print("账号格式错误")
+
+
+# python实现九九乘法表
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(i,"x",j,"=",i*j,end="")
+    print()
+
+'''
+
+for i in range(10):
+    if i==3:
+        break
+    print(i)
+
+for i in range(10):
+    if i==3:
+        continue
+    print(i)
